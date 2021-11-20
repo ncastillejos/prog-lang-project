c79b21515d8197e5efa6e71db67bb29881c3fb9c
Nancy Castillejos <ncastillejos1@gmail.com>
2021-11-19 16:14:32 -0800

    Update statscalc.py
    added & cleaned up comments

--- statscalc.py
+++ statscalc.py
@@ -1,14 +1,12 @@
 import csv
 
-with open('MOCK_DATA.csv', 'r') as csv_obj:
-    dict_reader = csv.DictReader(csv_obj)
+#open csv file in read mode
+with open('MOCK_DATA.csv', 'r') as csv_obj: 
+    # DictReader returns iterator
+    dict_reader = csv.DictReader(csv_obj)  
+
+    # iterator gets passed to list() & returns a list of dictionaries
     list_of_dict = list(dict_reader)
-#    for row in csv_data:
-#        if line_count == 0:
-#            print(f'Column names are {", ".join(row)}')
-#            line_count += 1
-#        else:
-#            print(f'\t{row[0]} , {row[1]} ')
-#            line_count += 1
+    
 print(*list_of_dict, sep="\n")
 print("# of list items: ", len(list_of_dict))
