e463b72dd3d4a4bec6639d869363a8d74208a0a7
Nancy Castillejos <ncastillejos1@gmail.com>
2021-11-19 17:44:04 -0800

    Update statscalc.py
    placed code to read csv file in a function

--- statscalc.py
+++ statscalc.py
@@ -1,12 +1,17 @@
 import csv
 
-#open csv file in read mode
-with open('MOCK_DATA.csv', 'r') as csv_obj: 
-    # DictReader returns iterator
-    dict_reader = csv.DictReader(csv_obj)  
 
-    # iterator gets passed to list() & returns a list of dictionaries
-    list_of_dict = list(dict_reader)
-    
-print(*list_of_dict, sep="\n")
-print("# of list items: ", len(list_of_dict))
+def filereader(file_path):
+    #open csv file in read mode
+    with open(file_path, 'r') as csv_obj: 
+        # DictReader returns iterator
+        dict_reader = csv.DictReader(csv_obj)  
+
+        # iterator gets passed to list() & returns a list of dictionaries
+        list_of_dict = list(dict_reader)
+    return list_of_dict
+
+d_list = filereader('MOCK_DATA.csv')
+
+print(*d_list, sep="\n")
+print("# of list items: ", len(d_list))
