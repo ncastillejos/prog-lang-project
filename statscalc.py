eb2b6fa035e2e01113969cc1005ff37ac4cd8f47
Nancy Castillejos <ncastillejos1@gmail.com>
2021-12-02 17:18:28 -0800

    updated statscalc
    non numerical data clean up implemented.

--- statscalc.py
+++ statscalc.py
@@ -11,7 +11,32 @@ def filereader(file_path):
         list_of_dict = list(dict_reader)
     return list_of_dict
 
+#def cleanup():
+def non_num(list):
+
+    # create a new list
+    # create a dictionary variable - this will be updated then appended to list 
+
+    new_list = []
+    new_dict = {}
+    #iterate over current list
+    for dict in list:
+        for key in dict:
+            #only append columns w numerical values
+            if(dict[key].isnumeric()):
+                new_dict.update({key:dict[key]})
+        #append dictionary as row in list                
+        new_list.append(new_dict.copy())
+        new_dict.clear()
+    return new_list
+
+                
+            
+    #print (list)
 d_list = filereader('MOCK_DATA.csv')
 
-print(*d_list, sep="\n")
-print("# of list items: ", len(d_list))
+#print(*d_list, sep="\n")
+#print("# of list items: ", len(d_list))
+
+upd_list = non_num(d_list)
+print(*upd_list, sep="\n")
