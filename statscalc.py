70ab25dda501bbd78fc54a6fbb2c98a01b9ec937
Nancy Castillejos <ncastillejos1@gmail.com>
2021-12-12 20:05:59 -0800

    Update statscalc.py
    added functions to remove duplicate rows rows with empty columns

--- statscalc.py
+++ statscalc.py
@@ -1,4 +1,6 @@
 import csv
+from collections import OrderedDict 
+from typing import OrderedDict
 
 
 def filereader(file_path):
@@ -30,13 +32,57 @@ def non_num(list):
         new_dict.clear()
     return new_list
 
-                
-            
-    #print (list)
+def rem_empty(list):
+    curr = 0
+    for dict in list:
+        lod = int(len(dict.keys()))
+        if lod > curr:
+           curr = lod
+    lim = range(len(list))
+
+    for index, item in enumerate(list):
+        #print(index, '\t', item, '\n')
+        lod = len(item.keys())
+        #print(lod)
+        
+        if curr > lod:
+            # print("pop: ", item, "\n")
+            list.pop(index)
+        
+    return list
+
+def rem_dups(list):
+    seen = set()
+    newlist = []
+    for dict in list:
+        d = tuple(dict.items())
+        if d not in seen:
+            seen.add(d)
+            newlist.append(d)
+
+    return newlist
+
+
+
+
 d_list = filereader('MOCK_DATA.csv')
+# d_list = filereader('Boston_Lyft_Uber_Data.csv')
 
 #print(*d_list, sep="\n")
 #print("# of list items: ", len(d_list))
 
+#update list with numerical vals in each col only
 upd_list = non_num(d_list)
-print(*upd_list, sep="\n")
+print("# of list1 items: ", len(upd_list))
+#print(*upd_list, sep="\n")
+
+#remove rows w empty 
+upd_list2 = rem_empty(upd_list)
+print("new list after cleanup: \n")
+print(*upd_list2, sep="\n")
+print("# of list2 items: ", len(upd_list2))
+
+print("\nnew list after cleanup: \n")
+upd_list3 = rem_dups(upd_list2)
+print("# of list3 items: ", len(upd_list3))
+print(*upd_list3, sep="\n")
